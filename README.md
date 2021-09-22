135d556e4cfdf06c64f76b0c698366a674e55462
dron22 <dron22@fastback.io>
2021-09-22 23:29:24 +0200

    Initial version

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 Github action for beautiful commit history :heart:
 
+![beautiful commit history example](images/beautiful-history.png?raw=true "Beautiful commit history")
+
+## Usage
+
+```yaml
+jobs:
+  ensure-rebase-flow:
+    name: Run rebase-flow-action
+    runs-on: ubuntu-latest
+    steps:
+      - name: "Checkout"
+        uses: actions/checkout@v2
+        with:
+          fetch-depth: 0
+      - name: "Ensure rebase flow for beautiful commit history"
+        uses: dron22/rebase-flow-action@v0.1.0
+        with:
+          target-branch: 'develop' # optionally specify a target branch (default: main)
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+
+async function run() {
+  try {
+    const targetBranch = core.getInput('target-branch');
+    
+    // fetch target branch commits
+    const targetBranchFullPath = `remotes/origin/${targetBranch}`
+    const commitsMainOutput = await executeFunction('git', ['rev-list', '--first-parent', targetBranchFullPath]);
+    const commitsMain = commitsMainOutput.split('\n');
+
+    // fetch current branch commits
+    const currentBanch = await executeFunction('git', ['branch'])
+    const commitsCurrentOutput = await executeFunction('git', ['rev-list', '--first-parent', currentBanch]);
+    const commitsCurrent = commitsCurrentOutput.split('\n');
+
+    // compare oldest ancestor and head of target
+    const oldestAncestor = commitsMain.filter(x => commitsCurrent.includes(x))[0];
+    const headOfMain = await executeFunction('git', ['rev-parse', targetBranchFullPath]);
+
+    if (oldestAncestor !== headOfMain) {
+      core.setFailed(`Current branch '${currentBanch}' needs to be rebased on target branch '${targetBranch}'`);
+    }
+  } catch (error) {
+    core.setFailed(`unexpected error: ${error.message}`);
+  }
+}
+
+const executeFunction = async(cmd, options) => {
+    let output = '';
+
+    const execOptions = {};
+    execOptions.listeners = {
+      stdout: (data) => {
+        output += data.toString();
+      },
+    };
+
+    await exec.exec(cmd, options, execOptions);
+
+    return output.replace(/^[ *\s]+|[ \s]+$/g, '');
+};
+
+run();
